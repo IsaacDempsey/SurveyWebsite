57e2b98f347d8137983993939f8c4b1b3f2c0dcd
JuliaBoes <juliaboes@posteo.de>
2018-11-09 10:50:17 +0000

    change graph style ala Tufte kinda

--- flaskServer.py
+++ flaskServer.py
@@ -11,7 +11,7 @@ from chartDict import chartDict
 app = Flask(__name__)
 
 # Start time (global)
-start = time.time() 
+start = time.time()
 
 # List of chart order. Default: [1, 2, ..., 24]
 c = [i for i in range(1, 25)]
@@ -19,24 +19,24 @@ c = [i for i in range(1, 25)]
 
 @app.route('/')
 def index():
-    """Renders index template."""  
-   
-    shuffle(c) # Randomise chart order
+    """Renders index template."""
+
+    shuffle(c)  # Randomise chart order
 
     return redirect('/chart/'+str(c[0]))
 
 
 @app.route('/chart/<int:chartNumber>')
 def chart(chartNumber):
-	"""Renders page containing chart of number specified.""" 
+    """Renders page containing chart of number specified."""
 
-	# Letters of alphabet
-	letters = list(string.ascii_uppercase)
+    # Letters of alphabet
+    letters = list(string.ascii_uppercase)
 
-	# Number of columns/bars in chart
-	data_points = len(chartDict[chartNumber]['data'])
+    # Number of columns/bars in chart
+    data_points = len(chartDict[chartNumber]['data'])
 
-	return render_template('SurveyWebsite.html', chartNumber=chartNumber, data_points=data_points, letters=letters)
+    return render_template('SurveyWebsite.html', chartNumber=chartNumber, data_points=data_points, letters=letters)
 
 
 @app.route('/data/<int:chartNumber>')
@@ -58,14 +58,14 @@ def results(chartNumber):
     columnSize = request.form.get('columnSize')
 
     print('Response Received. Chart: {0}, Column: {1}, Size: {2}, Duration: {3}'
-    	.format(chartNumber, column, columnSize, duration))
+          .format(chartNumber, column, columnSize, duration))
 
     with open('results.csv','a+') as file:
         file.write('{0},{1},{2},{3}\n'.format(chartNumber, column, columnSize, duration))
 
     # Redirect to finish page after all charts completed.
     if c.index(chartNumber) >= (len(c) - 1):
-    	return redirect(url_for('finish'))
+        return redirect(url_for('finish'))
 
     # Index of next chart number = (index of current element in list) + 1
     chartNumber = c[c.index(chartNumber) + 1]
@@ -75,11 +75,11 @@ def results(chartNumber):
 
 @app.route('/finish')
 def finish():
-	"""Finish page. Contains button that sends you back to index."""
+    """Finish page. Contains button that sends you back to index."""
 
-	return render_template('finish.html')
-	
-  
+    return render_template('finish.html')
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
